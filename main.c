b675f154e5ba63ad0765ae74022531eac3292c0c
wusunjie <wusunjie123@hotmail.com>
2015-11-15 11:21:41 +0800

    add test code for demo_widget.

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "system.h"
+#include "demo_widget.h"
 
 int main(void)
 {
 	system_boot();
 	printf("system boot successfully.\n");
+	demo_widget_init(demo_widget_ptr);
 	return 0;
 }
