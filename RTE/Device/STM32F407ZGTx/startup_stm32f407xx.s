93f3d2636cd8ec47234d9558d14709244435de59
wusunjie <wusunjie123@hotmail.com>
2015-11-15 17:50:36 +0800

    add support for external sram.

--- RTE/Device/STM32F407ZGTx/startup_stm32f407xx.s
+++ RTE/Device/STM32F407ZGTx/startup_stm32f407xx.s
@@ -49,7 +49,7 @@ Stack_Size      EQU     0x00000400
 
                 AREA    STACK, NOINIT, READWRITE, ALIGN=3
 Stack_Mem       SPACE   Stack_Size
-__initial_sp
+__initial_sp	EQU		0x20000000 + Stack_Size
 
 
 ; <h> Heap Configuration
